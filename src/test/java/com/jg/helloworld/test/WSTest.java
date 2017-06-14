427c1bddfea951883ef995e8a623a494c5d63da9
jg-7 <jg-7@users.noreply.github.com>
2017-06-14 01:50:37 -0400

    Improved unit tests
    -> added negative test

--- src/test/java/com/jg/helloworld/test/WSTest.java
+++ src/test/java/com/jg/helloworld/test/WSTest.java
@@ -1,6 +1,10 @@
 package com.jg.helloworld.test;
 
 import static org.junit.Assert.*;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import org.junit.Test;
 
 import com.sun.jersey.api.client.Client;
@@ -12,12 +16,22 @@ public class WSTest {
 	@Test
 	public void test() {
 		Client client = Client.create();
-		WebResource resource = client.resource("http://localhost:8080/hello-world/rest/hello-ws/this%20is%20a%20test");
+		String wsURL = "http://localhost:8080/hello-world/rest/hello-ws/";
+		String sText = "this is a test"; 
+
+		WebResource resource = client.resource(wsURL+sText.replaceAll("\\s", "%20"));
 		ClientResponse response = resource.get(ClientResponse.class);
 		
 		//Run the assertion
 		assertEquals("Should return 200 status code", 200, response.getStatus());
-		assertEquals("Return string must equal to input", "this is a test", response.getEntity(String.class));
+		assertEquals("Return string must equal to input", sText, response.getEntity(String.class));
+		
+		resource = client.resource(wsURL);
+		response = resource.get(ClientResponse.class);
+		
+		//Negative test
+		assertEquals("Should return 404 status code", 404, response.getStatus());
+
 	}
 
 }
