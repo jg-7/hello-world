6c745bf9d8d944ca1efbb65721b287fc39ecdbf1
jg-7 <jg-7@users.noreply.github.com>
2017-06-15 16:40:46 -0400

    Added plain jdbc

--- src/main/java/com/jg/helloworld/HelloWS.java
+++ src/main/java/com/jg/helloworld/HelloWS.java
@@ -4,6 +4,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import java.sql.Connection;
+
 import javax.ws.rs.GET;
 
 
@@ -12,6 +20,32 @@ public class HelloWS {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getText(@PathParam("param") String text){
+		//String txt = dbCon();
+		//System.out.println("txt="+txt);
 		return text;
+	}
+	
+	public String dbCon(){
+		String txt="";
+		int id=0;
+		String connURL = "jdbc:mysql://localhost:3306/jg_schema";
+		try{
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			Connection conn = DriverManager.getConnection(connURL, "root", "password");
+			PreparedStatement statement = conn.prepareStatement("SELECT * FROM messages");
+			ResultSet rs = statement.executeQuery();
+			if(rs!=null){
+				while(rs.next()){
+					id = rs.getInt("ID");
+					txt = rs.getString("TEXT");
+					//Date dt = rs.getDate("Created");
+				}
+			}
+			
+		}catch(Exception se){
+			System.out.println(se.getMessage());
+		}
+		return txt;
+		
 	}
 }
